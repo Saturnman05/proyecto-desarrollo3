c3b80ac76f1dce4cd9e630f6f3c164abd9cbaf56
Saturnman05 <franciscomaderanatera@gmail.com>
2024-07-24 02:44:05 -0400

    facturas

--- ProyectoVentas/Models/FacturaModel.cs
+++ ProyectoVentas/Models/FacturaModel.cs
@@ -7,5 +7,18 @@
         public DateTime EmissionDate { get; set; }
         public decimal TotalPrice { get; set; }
         public int UserId { get; set; }
+        public List<string> Productos { get; set; }
+
+        // TODO: Get factura by id
+
+        // TODO: Get facturas
+
+        // TODO: Get facturas by user
+
+        // TODO: Create factura
+
+        // TODO: Update factura
+
+        // TODO: Delete factura
     }
 }
