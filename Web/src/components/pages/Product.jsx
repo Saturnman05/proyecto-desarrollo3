cd7cf80c55400184b7ba70152229b0948a868186
fmn <franciscomaderanatera@gmail.com>
2024-09-15 01:42:10 -0400

    refactor Product

--- Web/src/components/pages/Product.jsx
+++ Web/src/components/pages/Product.jsx
@@ -1,25 +1,8 @@
-import { useParams } from 'react-router-dom'
-import { API_URL } from '../../constants/constantes'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
+import { useProduct } from '../../hooks/useProducts'
 
 export default function Product() {
-  const { productId } = useParams()
-  const [product, setProduct] = useState(null)
-
-  const getProduct = async () => {
-    try {
-      const response = await fetch(`${API_URL}api/Products/${productId}`)
-
-      if (!response.ok) {
-        throw new Error('Error al obtener los datos del producto')
-      }
-
-      const productData = await response.json()
-      setProduct(productData)
-    } catch (error) {
-      console.log('Error:', error)
-    }
-  }
+  const { product, getProduct } = useProduct()
 
   useEffect(() => {
     getProduct()
--- Web/src/hooks/useProducts.js
+++ Web/src/hooks/useProducts.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
-import { API_URL } from "../constants/constantes";
+import { useState } from 'react'
+import { API_URL } from '../constants/constantes'
+import { useParams } from 'react-router-dom'
 
 export function useProducts () {
   const [products, setProducts] = useState([])
@@ -22,4 +23,26 @@ export function useProducts () {
   }
 
   return { products, setProducts, loadProducts }
+}
+
+export function useProduct () {
+  const { productId } = useParams()
+  const [product, setProduct] = useState(null)
+
+  const getProduct = async () => {
+    try {
+      const response = await fetch(`${API_URL}api/Products/${productId}`)
+
+      if (!response.ok) {
+        throw new Error('Error al obtener los datos del producto')
+      }
+
+      const productData = await response.json()
+      setProduct(productData)
+    } catch (error) {
+      console.log('Error:', error)
+    }
+  }
+
+  return { product, getProduct }
 }
