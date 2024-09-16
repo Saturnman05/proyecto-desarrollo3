8692326efe417a75559aee14f65c658b7e956bb4
fmn <franciscomaderanatera@gmail.com>
2024-09-16 17:47:10 -0400

    ahora si funciona como una spa

--- Web/src/components/App.jsx
+++ Web/src/components/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import NavComponent from './NavComponent'
 
+import Cart from './pages/Cart'
 import Login from './pages/Login'
 import PaginaInicio from './pages/PaginaInicio'
 import Product from './pages/Product'
@@ -10,13 +11,14 @@ import Register from './pages/Register'
 export default function App () {
   return (
     <>
-      <NavComponent />
       <Router>
+        <NavComponent />
         <Routes>
           <Route path="/" element={<PaginaInicio />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/product/:productId" element={<Product />} />
+          <Route path="/cart/:userId" element={<Cart />} /> 
         </Routes>
       </Router>
     </>
--- Web/src/components/NavComponent.jsx
+++ Web/src/components/NavComponent.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { Button, Container, Form, Nav, Navbar, NavDropdown, Offcanvas } from 'react-bootstrap'
 import { useLogout } from '../hooks/useLogout'
@@ -11,55 +12,58 @@ export default function NavComponent () {
   }, [userVal])
 
   return (
-    <Navbar key={"xxl"} expand={"xxl"} className="bg-body-tertiary mb-3">
+    <Navbar key={'xxl'} expand={'xxl'} className='bg-body-tertiary mb-3'>
       <Container fluid>
-        <Navbar.Brand href="/">CompraNet</Navbar.Brand>
-        <Navbar.Toggle aria-controls="offcanvasNavbar-expand-xxl" />
+        <Navbar.Brand as={Link} to='/'>CompraNet</Navbar.Brand>
+        <Navbar.Toggle aria-controls='offcanvasNavbar-expand-xxl' />
         <Navbar.Offcanvas
-          id="offcanvasNavbar-expand-xxl"
-          aria-labelledby="offcanvasNavbarLabel-expand-xxl"
-          placement="end"
+          id='offcanvasNavbar-expand-xxl'
+          aria-labelledby='offcanvasNavbarLabel-expand-xxl'
+          placement='end'
         >
           <Offcanvas.Header closeButton>
-            <Offcanvas.Title id="offcanvasNavbarLabel-expand-xxl">
+            <Offcanvas.Title id='offcanvasNavbarLabel-expand-xxl'>
               {menuMessage}
             </Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
-            <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Nav.Link href="/">Home</Nav.Link>
+            <Nav className='justify-content-end flex-grow-1 pe-3'>
+              <Nav.Link as={Link} to='/'>Home</Nav.Link>
               {
                 (Number.isInteger(userVal.userId) && userVal.userId > 0) ? (
-                  <Nav.Link href="/" onClick={() => logout()}>Log Out</Nav.Link>
+                  <>
+                    <Nav.Link as={Link} to={`cart/${userVal.userId}`}>My Cart</Nav.Link>
+                    <Nav.Link as={Link} to='/' onClick={() => logout()}>Log Out</Nav.Link>
+                  </>
                 ) : (
                   <>
-                    <Nav.Link href="/login">Log In</Nav.Link>
-                    <Nav.Link href='/register'>Register</Nav.Link>
+                    <Nav.Link as={Link} to='/login'>Log In</Nav.Link>
+                    <Nav.Link as={Link} to='/register'>Register</Nav.Link>
                   </>
                 )
               }
               <NavDropdown
-                title="Dropdown"
-                id="offcanvasNavbarDropdown-expand-xxl"
+                title='Dropdown'
+                id='offcanvasNavbarDropdown-expand-xxl'
               >
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
+                <NavDropdown.Item href='#action3'>Action</NavDropdown.Item>
+                <NavDropdown.Item href='#action4'>
                   Another action
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
+                <NavDropdown.Item href='#action5'>
                   Something else here
                 </NavDropdown.Item>
               </NavDropdown>
             </Nav>
-            <Form className="d-flex">
+            <Form className='d-flex'>
               <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
+                type='search'
+                placeholder='Search'
+                className='me-2'
+                aria-label='Search'
               />
-              <Button variant="outline-success" onClick={() => { console.log(userVal); console.log(userVal.userId) }}>Search</Button>
+              <Button variant='outline-success' onClick={() => { console.log(userVal); console.log(userVal.userId) }}>Search</Button>
             </Form>
           </Offcanvas.Body>
         </Navbar.Offcanvas>
