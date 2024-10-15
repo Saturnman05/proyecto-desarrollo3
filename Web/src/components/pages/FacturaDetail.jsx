2be7a34fe6a4f5cb06413a69d44ae5db9f35f8e5
fmn <franciscomaderanatera@gmail.com>
2024-10-15 12:42:19 -0400

    factura detail

--- Web/src/components/pages/FacturaDetail.jsx
+++ Web/src/components/pages/FacturaDetail.jsx
@@ -1,28 +1,43 @@
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { useFactura } from "../../hooks/useFactura"
 import { useEffect } from "react"
-import { Container } from "react-bootstrap"
+import { Container, Spinner } from "react-bootstrap"
 
 export default function FacturaDetail () {
-  const { facturas, getFacturaById } = useFactura()
+  const { facturas, getFacturaById, getProductsFromFactura, products } = useFactura()
   const { facturaId } = useParams()
 
   useEffect(() => {
     getFacturaById(facturaId)
+    getProductsFromFactura(facturaId)
   }, [])
   
   return (
     <Container className='flex-grow-1 py-2'>
+      <Link to='/bought'>Back</Link>
       <h1>Purchase Detail</h1>
-      <p>Bill no. {facturas?.facturaId}</p>
+      <p>Bill no. {facturas?.facturaId || 'Loading...'}</p> {/* Verificación de existencia o mensaje */}
+      
       <p>Products:</p>
-      <ul>
-        {
-          facturas?.productos?.map((producto, index) => (
-            <li key={index}>{producto}</li>
-          ))
-        }
-      </ul>
+      {
+        products.length === 0 ? (
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        ) : (
+          <ul>
+            {products.map((producto, index) => (
+              <li key={index}>
+                <Link to={`/product/${producto.productId}`}><h5>{producto.name}</h5></Link>
+                <p>Description: {producto.description}</p>
+                <p>Price: ${producto.unitPrice}</p>
+                <img src={producto.imageUrl} alt={producto.name} style={{ width: "100px" }} />
+              </li>
+            ))}
+          </ul>
+        )
+      }
+      <p>Total: ${facturas.totalPrice}</p>
     </Container>
   )
 }
--- Web/src/hooks/useFactura.js
+++ Web/src/hooks/useFactura.js
@@ -1,10 +1,14 @@
 import { useContext, useState } from 'react'
 import { UserContext } from '../context/user'
 import { API_URL } from '../constants/constantes'
+import { useProduct } from './useProducts'
 
 export function useFactura () {
   const { userVal } = useContext(UserContext)
   const [facturas, setFacturas] = useState([])
+  const [products, setProducts] = useState([])
+
+  const { getProduct } = useProduct()
 
   const getFacturaUsuario = async () => {
     try {
@@ -43,5 +47,33 @@ export function useFactura () {
     }
   }
 
-  return { facturas, getFacturaUsuario, getFacturaById }
+  const getProductsFromFactura = async (facturaId) => {
+    try {
+      const response = await fetch(`${API_URL}api/Facturas/facturaproduct/${facturaId}`)
+      const productsJson = await response.json()
+
+      const returnProducts = await Promise.all(
+        productsJson.map(async (product) => {
+          const gProduct = await getProduct(product.productId)
+
+          return {
+            productId: product.productId,
+            name: gProduct.name,
+            description: gProduct.description,
+            imageUrl: gProduct.imageUrl,
+            unitPrice: product.productUnitPrice,
+            stock: gProduct.stock,
+            dateCreated: gProduct.dateCreated,
+            userId: gProduct.userId
+          }
+        })
+      )
+
+      setProducts(returnProducts)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return { facturas, getFacturaUsuario, getFacturaById, getProductsFromFactura, products }
 }
