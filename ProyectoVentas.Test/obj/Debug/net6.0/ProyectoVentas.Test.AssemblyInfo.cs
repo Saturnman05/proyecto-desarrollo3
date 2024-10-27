03840593e3046e412fbb2beadd5a51e649a19aa1
fran <franciscomaderanatera@gmail.com>
2024-10-27 01:09:45 -0400

    api desplegada

--- ProyectoVentas.Test/obj/Debug/net6.0/ProyectoVentas.Test.AssemblyInfo.cs
+++ ProyectoVentas.Test/obj/Debug/net6.0/ProyectoVentas.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoVentas.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f87c9464d2e143a0867048234e085647ae85e3a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+916736cb4c78a2cfb551dfe48b829838bc8957c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoVentas.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoVentas.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProyectoVentas/obj/Debug/net6.0/ProyectoVentas.AssemblyInfo.cs
+++ ProyectoVentas/obj/Debug/net6.0/ProyectoVentas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoVentas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f190f231f919570bde90c538abc2e10d178c90e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+916736cb4c78a2cfb551dfe48b829838bc8957c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoVentas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoVentas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
